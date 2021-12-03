090b84132c6cadd5c8c7b8ce3edec9b6bcd0dd52
Kei Kamikawa <code-hex@users.noreply.github.com>
2021-12-03 12:30:02 +0900

    fixed some code

--- cache.go
+++ cache.go
@@ -3,44 +3,34 @@ package cache
 import (
 	"sync"
 	"time"
+
+	"github.com/Code-Hex/go-generics-cache/lfu"
+	"github.com/Code-Hex/go-generics-cache/lru"
+	"github.com/Code-Hex/go-generics-cache/simple"
 )
 
-// Cache is a common-cache interface.
-type Cache[K comparable, V any] interface {
+// Interface is a common-cache interface.
+type Interface[K comparable, V any] interface {
 	Get(key K) (value V, ok bool)
-	Set(key K, val V, opts ...ItemOption)
+	Set(key K, val V)
 	Keys() []K
 	Delete(key K)
-	Contains(key K) bool
 }
+
+var (
+	_ Interface[any, any] = (*simple.Cache[any, any])(nil)
+	_ Interface[any, any] = (*lru.Cache[any, any])(nil)
+	_ Interface[any, any] = (*lfu.Cache[any, any])(nil)
+)
 
 // Item is an item
 type Item[K comparable, V any] struct {
-	Key            K
-	Value          V
-	ReferenceCount int
-	Expiration     time.Duration
-	CreatedAt      time.Time
-	ReferencedAt   time.Time
-}
-
-// Referenced increments a reference counter and updates `ReferencedAt`
-// to current time.
-func (i *Item[K, V]) Referenced() {
-	i.ReferenceCount++
-	i.ReferencedAt = nowFunc()
+	Key        K
+	Value      V
+	Expiration time.Duration
 }
 
 var nowFunc = time.Now
-
-// HasExpired returns true if the item has expired.
-// If the item's expiration is zero value, returns false.
-func (i Item[K, T]) HasExpired() bool {
-	if i.Expiration <= 0 {
-		return false
-	}
-	return i.CreatedAt.Add(i.Expiration).Before(nowFunc())
-}
 
 // ItemOption is an option for cache item.
 type ItemOption func(*itemOptions)
@@ -50,42 +40,155 @@ type itemOptions struct {
 }
 
 // WithExpiration is an option to set expiration time for any items.
+// If the expiration is zero or negative value, it treat as no expiration.
 func WithExpiration(exp time.Duration) ItemOption {
 	return func(o *itemOptions) {
 		o.expiration = exp
 	}
 }
 
-// NewItem creates a new item with specified any options.
-func NewItem[K comparable, V any](key K, val V, opts ...ItemOption) *Item[K, V] {
+// newItem creates a new item with specified any options.
+func newItem[K comparable, V any](key K, val V, opts ...ItemOption) *Item[K, V] {
 	o := new(itemOptions)
 	for _, optFunc := range opts {
 		optFunc(o)
 	}
-	now := nowFunc()
 	return &Item[K, V]{
-		Key:            key,
-		Value:          val,
-		ReferenceCount: 1,
-		Expiration:     o.expiration,
-		CreatedAt:      now,
-		ReferencedAt:   now,
+		Key:        key,
+		Value:      val,
+		Expiration: o.expiration,
 	}
+}
+
+// Cache is a cache.
+type Cache[K comparable, V any] struct {
+	cache       Interface[K, *Item[K, V]]
+	expirations map[K]chan struct{}
+	// mu is used to do lock in some method process.
+	mu sync.RWMutex
+}
+
+// Option is an option for cache.
+type Option[K comparable, V any] func(*options[K, V])
+
+type options[K comparable, V any] struct {
+	cache Interface[K, *Item[K, V]]
+}
+
+func newOptions[K comparable, V any]() *options[K, V] {
+	return &options[K, V]{
+		cache: simple.NewCache[K, *Item[K, V]](),
+	}
+}
+
+// AsLRU is an option to make a new Cache as LRU algorithm.
+func AsLRU[K comparable, V any](cap int) Option[K, V] {
+	return func(o *options[K, V]) {
+		o.cache = lru.NewCacheWithCap[K, *Item[K, V]](cap)
+	}
+}
+
+// AsLFU is an option to make a new Cache as LFU algorithm.
+func AsLFU[K comparable, V any](cap int) Option[K, V] {
+	return func(o *options[K, V]) {
+		o.cache = lfu.NewCacheWithCap[K, *Item[K, V]](cap)
+	}
+}
+
+// New creates a new Cache.
+func New[K comparable, V any](opts ...Option[K, V]) *Cache[K, V] {
+	o := newOptions[K, V]()
+	for _, optFunc := range opts {
+		optFunc(o)
+	}
+	return &Cache[K, V]{
+		cache:       o.cache,
+		expirations: make(map[K]chan struct{}, 0),
+	}
+}
+
+// Get looks up a key's value from the cache.
+func (c *Cache[K, V]) Get(key K) (value V, ok bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	item, ok := c.cache.Get(key)
+	if !ok {
+		return
+	}
+	return item.Value, true
+}
+
+// Set sets a value to the cache with key. replacing any existing value.
+func (c *Cache[K, V]) Set(key K, val V, opts ...ItemOption) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	item := newItem(key, val, opts...)
+	if item.Expiration <= 0 {
+		c.cache.Set(key, item)
+		return
+	}
+
+	if _, ok := c.cache.Get(key); ok {
+		c.doneWatchExpiration(key)
+	}
+
+	c.cache.Set(key, item)
+	c.installExpirationWatcher(item.Key, item.Expiration)
+}
+
+func (c *Cache[K, V]) installExpirationWatcher(key K, exp time.Duration) {
+	done := make(chan struct{})
+	c.expirations[key] = done
+	go func() {
+		select {
+		case <-time.After(exp):
+			c.Delete(key)
+		case <-done:
+		}
+	}()
+}
+
+func (c *Cache[K, V]) doneWatchExpiration(key K) {
+	if ch, ok := c.expirations[key]; ok {
+		close(ch)
+	}
+}
+
+// Keys returns the keys of the cache. the order is relied on algorithms.
+func (c *Cache[K, V]) Keys() []K {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.cache.Keys()
+}
+
+// Delete deletes the item with provided key from the cache.
+func (c *Cache[K, V]) Delete(key K) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache.Delete(key)
+}
+
+// Contains reports whether key is within cache.
+func (c *Cache[K, V]) Contains(key K) bool {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	_, ok := c.cache.Get(key)
+	return ok
 }
 
 // NumberCache is a in-memory cache which is able to store only Number constraint.
 type NumberCache[K comparable, V Number] struct {
-	Cache[K, V]
+	*Cache[K, V]
 	// nmu is used to do lock in Increment/Decrement process.
-	// Note that this must be here as a separate mutex because mu in Cache struct is Locked in GetItem,
+	// Note that this must be here as a separate mutex because mu in Cache struct is Locked in Get,
 	// and if we call mu.Lock in Increment/Decrement, it will cause deadlock.
 	nmu sync.Mutex
 }
 
 // NewNumber creates a new cache for Number constraint.
-func NewNumber[K comparable, V Number](baseCache Cache[K, V]) *NumberCache[K, V] {
+func NewNumber[K comparable, V Number](opts ...Option[K, V]) *NumberCache[K, V] {
 	return &NumberCache[K, V]{
-		Cache: baseCache,
+		Cache: New(opts...),
 	}
 }
 
